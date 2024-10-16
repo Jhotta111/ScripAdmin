c4fa6f137e26879a3150d7834a99c48a1918fbcb
Jhotta111 <vascosjhotta11@gmail.com>
2024-10-16 18:32:45 +0200

    primera edicion del index

---
+++ procesos.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+
+header("location:index.html");
+
+
+
+
+
+
+
+
+
+
+
+
+//Nombre de usuario = usun
+//Contraseña = clave
+//Unidad Organizativa = unor
+//Nombre del dominio = ndom
+//Extension del dominio = exdom
+//El usuario puede cambiar la clave = pcc (si o no)
+//El suario será admin? = tipus (stan admin)
+// Activarlo inmediatamente = aimed (si o no)
+
+// Hacer o descarcartar como admin o añadir a un grupo = hdcm (had o dca o agrp)
+
+// Nombre del grupo = ngrp 
+
+//Tipo de grupos = tdg
+
+//Grupo seguro = gsec (sec o nsec)
+
+
+//Crear carpetas = ccarp
+//Borrar carpetas = bcarp 
+
+
+
+//Nombre de la Unidad Organizativa = nuo
+
+
+
+
+
+?>
